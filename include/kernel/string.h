c5ae655251e4e6f1a5d6e30785c4fd7a7f88fe1e
Loïc Buckwell <lfalkau@student.42.fr>
2022-10-12 14:34:28 +0200

    Create a standalone string library
    This string library contains some basic memory/string manipulation.
    It is NOT YET tested, nor reviewed, so make use of it with caution.
    
    Signed-off-by: Loïc Buckwell <lfalkau@student.42.fr>

---
+++ include/kernel/string.h
@@ -0,0 +1,36 @@
+#ifndef STRING_H
+#define STRING_H
+
+#include <stddef.h>
+
+/*
+	The string library defines some basic string/memory manipulation functions.
+	They are almost all named after the glibc ones, thus you can see their
+	respective manual from your host.
+*/
+
+/* lib/string/str.c */
+size_t	strlen(const char *s);
+char	*strchr(const char *s, int c);
+char	*strrchr(const char *s, int c);
+int		strcmp(const char *s1, const char *s2);
+int		strncmp(const char *s1, const char *s2, size_t n);
+char	*strcpy(char *dest, const char *src);
+char	*strncpy(char *dest, const char *src, size_t n);
+size_t	strspn(const char *s, const char *accept);
+size_t	strcspn(const char *s, const char *reject);
+char	*strpbrk(const char *s, const char *accept);
+char	*strstr(const char *haystack, const char *needle);
+char	*strtok(char *str, const char *delim);
+char	*strcat(char *dst, const char *src);
+char	*strncat(char *dst, const char * src, size_t n);
+
+/* lib/string/mem.c */
+void	*memchr(const void *s, int c, size_t n);
+void	*memrchr(const void *s, int c, size_t n);
+int		memcmp(const void *s1, const void *s2, size_t n);
+void	*memcpy(void *dst, const void *src, size_t n);
+void	*memmove(void *dst, const void *src, size_t n);
+void	*memset(void *s, int c, size_t n);
+
+#endif
---
+++ lib/string/mem.c
@@ -0,0 +1,70 @@
+#include <stddef.h>
+
+void *memchr(const void *s, int c, size_t n) {
+	unsigned char *sc = (unsigned char *)s;
+
+	while (n--) {
+		if (*sc == c)
+			return (void *)sc;
+		sc++;
+	}
+	return NULL;
+}
+
+void *memrchr(const void *s, int c, size_t n) {
+	unsigned char *sc = (unsigned char *)s;
+
+	sc += n;
+	while (n--) {
+		if (*sc == c)
+			return (void *)sc;
+		sc--;
+	}
+	return NULL;
+}
+
+int  memcmp(const void *s1, const void *s2, size_t n) {
+	unsigned char *sc1 = (unsigned char *)s1;
+	unsigned char *sc2 = (unsigned char *)s2;
+
+	while (n > 0 && *sc1 == *sc2) {
+		sc1++, sc2++, n--;
+	}
+	return n == 0 ? 0 : *sc1 - *sc2;
+}
+
+void *memcpy(void *dst, const void *src, size_t n) {
+	void *ptr = dst;
+
+	while (n--) {
+		*(unsigned char *)dst = *(unsigned char *)src;
+		dst++, src++;
+	}
+	return ptr;
+}
+
+void *memmove(void *dst, const void *src, size_t n) {
+	unsigned char *dstc = dst;
+	const unsigned char *srcc = src;
+	void *ptr = dst;
+
+	while (n--) {
+		if (dstc > srcc) {
+			*(dstc + n) = *(srcc + n);
+		} else {
+			*dstc = *srcc;
+			dstc++, srcc++;
+		}
+	}
+	return ptr;
+}
+
+void *memset(void *s, int c, size_t n) {
+	unsigned char *sc = s;
+	void *ptr = s;
+
+	while (n--) {
+		*sc++ = c;
+	}
+	return ptr;
+}
