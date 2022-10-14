f3325325130f271927dbd246b43cdb7e336a8d79
xlmod <glafond-@student.42.fr>
2022-10-14 14:30:52 +0200

    Create interface to read/write to port.
    Make two function port_read and port_write to read and write in given
    port.
    
    Signed-off-by: xlmod <glafond-@student.42.fr>

---
+++ kernel/port.c
@@ -0,0 +1,29 @@
+/* kernel/port.c
+ *
+ * Interface to read and write to port.
+ *
+ * created: 2022/10/14 - xlmod <glafond-@student.42.fr>
+ * updated: 2022/10/14 - xlmod <glafond-@student.42.fr>
+ */
+
+#include <stdint.h>
+
+/* Wrapper to asm instruction 'in'
+ *
+ * @arg(port): port number to read on.
+ * @ret: value read.
+ */
+uint8_t	port_read(uint16_t port) {
+	uint8_t val;
+	asm inline volatile ("inb %1, %0" : "=a"(val) : "Nd"(port));
+	return val;
+}
+
+/* Wrapper to asm instruction 'out'
+ *
+ * @arg(port): port number to write on.
+ * @arg(val): value to write.
+ */
+void port_write(uint16_t port, uint8_t val) {
+	asm inline volatile ("outb %0, %1" : : "a"(val), "Nd"(port));
+}
