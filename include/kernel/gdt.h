7668a67ca65af186c2f5b2559084f90faebc106b
mrxx0 <chcoutur@student.42.fr>
2022-10-19 09:47:34 +0200

    Update general norm
    Remove empty lines in function and between exported and private headers.
    Adapt comments style to their child (functions, structs...)
    
    Signed-off-by: mrxx0 <chcoutur@student.42.fr>

--- include/kernel/gdt.h
+++ include/kernel/gdt.h
@@ -2,17 +2,15 @@
  *
  *
  * created: 2022/10/18 - mrxx0 <chcoutur@student.42.fr>
- * updated: 2022/10/18 - mrxx0 <chcoutur@student.42.fr>
+ * updated: 2022/10/19 - mrxx0 <chcoutur@student.42.fr>
  */
-
 
 #ifndef GDT_H
 #define GDT_H
 
 #define GDT_BASE 0x800
 
-/* Initialize the gdt and load it.
- */
+/* Initialize the gdt and load it. */
 void gdt_init();
 
 #endif
