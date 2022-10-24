ec06d747ed116a744887095a4c30819254b6b5d5
xlmod <glafond-@student.42.fr>
2022-10-24 13:08:48 +0200

    Add cursor handling and '\t' and '\b'
    Update cursor when write charactere.
    Enable or disable cursor when cursor is on the screen or not.
    '\t': add between 1-4 space to put cursor on mod 4 value.
    '\b': remove th last charactere, if the start of the line put the cursor
    at the last charactere of the previous line.
    
    Signed-off-by: xlmod <glafond-@student.42.fr>

--- include/kernel/screenbuf.h
+++ include/kernel/screenbuf.h
@@ -11,7 +11,12 @@
 
 #include <kernel/vga.h>
 
-#define SCREENBUF_HEIGHT	VGA_HEIGHT * 8
+#define SB_HEIGHT		VGA_HEIGHT * 8
+
+#define SB_WHITESPACE	(uint16_t)0x0f00
+
+#define SB_CURSOR_PORT1	0x3D4
+#define SB_CURSOR_PORT2	0x3D5
 
 struct screenbuf {
 	uint16_t		*top;
@@ -21,7 +26,7 @@ struct screenbuf {
 	uint16_t		cursor_offset;
 	uint8_t			color;
 	int				loaded;
-	uint16_t		buf[VGA_WIDTH * SCREENBUF_HEIGHT];
+	uint16_t		buf[VGA_WIDTH * SB_HEIGHT];
 };
 
 void sb_init(struct screenbuf *sb);
