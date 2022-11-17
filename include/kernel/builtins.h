39dba53e2ff3288a24a304f223d1a8a02df4f91c
mrxx0 <chcoutur@student.42.fr>
2022-11-17 15:48:02 +0100

    Implement a basic shell with builtins commands.
    This shell replace the menu from kernel.c
    It handles basic commands listed in include/kernel/builtins.h and kernel/builtins.c
    
    Signed-off-by: mrxx0 <chcoutur@student.42.fr>

---
+++ include/kernel/builtins.h
@@ -0,0 +1,25 @@
+// SPDX-FileCopyrightText: CGL-KFS
+// SPDX-License-Identifier: BSD-3-Clause
+
+/* builtins.h
+ *
+ * Builtins functions for shell.
+ *
+ * created: 2022/11/17 - mrxx0 <chcoutur@student.42.fr>
+ * updated: 2022/11/17 - mrxx0 <chcoutur@student.42.fr>
+ */
+
+#ifndef BUILTINS_H
+#define BUILTINS_H
+
+void reboot();
+void shell();
+void key_us();
+void key_fr();
+void color_red();
+void color_reset();
+void clear();
+void help();
+void int_divide();
+
+#endif
