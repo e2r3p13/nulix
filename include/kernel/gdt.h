52bacffeffe274ff27a5cc991a04c73143e5f2b1
mrxx0 <chcoutur@student.42.fr>
2022-11-18 11:20:39 +0100

    Use define for all GDT entries.
    Replace hardcoded values with define for all values in the GDT entries.
    Each value is either the result of a simple define or of multiple
    operations between define.
    The define of the GDT address 'GDT_BASE' (now GDT_BASE_ADDRESS) has been moved to gdt.c as it is not used
    in other files.
    
    Signed-off-by: mrxx0 <chcoutur@student.42.fr>

--- include/kernel/gdt.h
+++ include/kernel/gdt.h
@@ -6,13 +6,12 @@
  * Global descriptor table header file
  *
  * created: 2022/10/18 - mrxx0 <chcoutur@student.42.fr>
- * updated: 2022/10/26 - mrxx0 <chcoutur@student.42.fr>
+ * updated: 2022/11/18 - mrxx0 <chcoutur@student.42.fr>
  */
 
 #ifndef GDT_H
 #define GDT_H
 
-#define GDT_BASE 0x800
 /* Initialize the gdt and load it. */
 void gdt_init();
 
