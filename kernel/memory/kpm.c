97afc2ca45937068095634f43f7f2d66b746176a
lfalkau <lfalkau@student.42.fr>
2022-12-06 13:15:38 +0100

    Implements the alloc and free functions
    kpm_alloc:
    
    The alloc function takes a size and will
    determine the smallest order that can hold
    this size.
    
    It will then go through the bitmap of that order
    to find a free block. If one is found, it will
    allocate every child nodes on the first order,
    fill the chunk structure, and update the parents
    thanks to the kpm_update_orders function.
    
    If none is found, it will decrement the order in which
    it searches and try to find a smaller block. This operation
    is repeated until the smallest order, at which point if no
    block is found, it means that no more memory is available,
    and the function returns an error code.
    
    kpm_free:
    
    The free function first checks that the provided chunk is aligned.
    It then computes the frame index and the number of frames to free.
    Now, for each frame, it will first check that the zone is enabled,
    to avoid making a disabled zone available for future malloc calls
    and free it if it is.
    Finally, kpm_update_orders will be called to reflect the changes on
    the whole bitmap.
    
    Signed-off-by: lfalkau <lfalkau@student.42.fr>

--- kernel/memory/kpm.c
+++ kernel/memory/kpm.c
@@ -6,7 +6,7 @@
  * Kernel Physical Memory management
  *
  * created: 2022/11/23 - lfalkau <lfalkau@student.42.fr>
- * updated: 2022/12/02 - lfalkau <lfalkau@student.42.fr>
+ * updated: 2022/12/08 - lfalkau <lfalkau@student.42.fr>
  */
 
 #include <kernel/kpm.h>
@@ -32,15 +32,6 @@ void buddy_print() {
 	VGA_setforegroundcolor(VGA_DFL_COLOR);
 }
 
-static inline size_t compute_orders_size() {
-	size_t nbytes = 0;
-
-	for (size_t i = 0, nblocks = buddy->nframes; i < KPM_NORDERS; i++, nblocks /= 2) {
-		nbytes += KPM_NBYTES_FROM_NBITS(nblocks);
-	}
-	return nbytes;
-}
-
 /*
  * kpm_init must be called before any call to other kpm functions.
  * It initializes the buddy structure in memory and set the available
@@ -59,17 +50,21 @@ void kpm_init(struct multiboot_mmap_entry *entries, size_t count, size_t memkb) 
 	buddy = (buddy_t *)ALIGNNEXT((uint32_t)&ek, PAGE_SIZE);
 	buddy->nframes = ALIGN(memkb * 1024 / PAGE_SIZE, 1024);
 	enabled_frames_size = KPM_NBYTES_FROM_NBITS(buddy->nframes / 8);
-	orders_size = compute_orders_size();
+	orders_size = 0;
+	for (size_t i = 0, nblocks = buddy->nframes; i < KPM_NORDERS; i++, nblocks /= 2) {
+		size_t order_size = KPM_NBYTES_FROM_NBITS(nblocks);
+		buddy->orders[i].size = orders_size;
+		orders_size += order_size;
+	}
 	buddy->size = sizeof(buddy_t) + enabled_frames_size + orders_size;
 	buddy->enabled_frames = (void *)buddy + sizeof(buddy_t);
 
-	buddy->orders[0] = (void *)buddy->enabled_frames + enabled_frames_size;
-	for (size_t i = 1, prev_nblocks = buddy->nframes; i < KPM_NORDERS; i++, prev_nblocks /= 2) {
-		buddy->orders[i] = (void *)buddy->orders[i - 1] + KPM_NBYTES_FROM_NBITS(prev_nblocks);
-	}
+	buddy->orders[0].bitmap = (void *)buddy->enabled_frames + enabled_frames_size;
+	for (size_t i = 1, prev_nblocks = buddy->nframes; i < KPM_NORDERS; i++, prev_nblocks /= 2)
+		buddy->orders[i].bitmap = (void *)buddy->orders[i - 1].bitmap + KPM_NBYTES_FROM_NBITS(prev_nblocks);
 
 	memset(buddy->enabled_frames, 0, enabled_frames_size);
-	memset(buddy->orders[0], 0xff, orders_size);
+	memset(buddy->orders[0].bitmap, 0xff, orders_size);
 
 	for (struct multiboot_mmap_entry *entry = entries; entry->size != 0; entry++) {
 		if (entry->type == MULTIBOOT_MEMORY_AVAILABLE)
@@ -176,10 +171,83 @@ int kpm_isalloc(void *addr) {
 	return KPM_IS_ALLOCATED(0, (uintptr_t)addr / PAGE_SIZE);
 }
 
-void *kpm_alloc(size_t size) {
+static int find_best_fit_order(size_t size) {
+	size_t nframes;
 
+	nframes = ALIGNNEXT(size, PAGE_SIZE) / PAGE_SIZE;
+	for (int n = 0; n < KPM_NORDERS; n++) {
+		size_t nframes_per_block = 1 << n;
+		if (nframes_per_block >= nframes)
+			return n;
+	}
+	return KPM_NORDERS - 1;
 }
 
-void kpm_free(void *addr) {
+/*
+ * Search for a bit set to 0 in a bitmap composed
+ * of several words.
+ *
+ * Returns the index of a bit set to 0 if found, 0 otherwise
+ */
+static int bitmap_ffu(bitmap_t *bitmap, size_t size) {
+	int ffu;
 
+	for (size_t i = 0; i < size; i++) {
+		bitmap_t *cur = bitmap + i;
+		ffu = __builtin_ffs(~*cur);
+		if (ffu)
+			return ffu + (i * 8) - 1;
+	}
+	return -1;
+}
+
+/*
+ * kpm_alloc searches for the biggest contiguous region, up to
+ * @size bytes, and fills the struct @chunk with this candidate.
+ *
+ * Returns 0 on success, -1 on error
+ *
+ * NOTE: the returned chunk may be smaller than the requested size if there
+ * is no contiguous block big enough. In this case, subsequent calls will
+ * be needed to get the remaining chunks.
+ */
+int kpm_alloc(kpm_chunk_t *chunk, size_t size) {
+	size_t best_fit_order;
+	size_t base_index;
+	size_t frames_per_block;
+
+	best_fit_order = find_best_fit_order(size);
+	for (size_t n = best_fit_order; n > 0; n--) {
+		int i = bitmap_ffu(buddy->orders[n].bitmap, buddy->orders[n].size);
+		if (i >= 0) {
+			frames_per_block = 1 << n;
+			base_index = i * frames_per_block;
+			for (size_t j = 0; j < frames_per_block; j++)
+				KPM_ALLOC(0, base_index + j);
+			chunk->addr = (void *)(i * PAGE_SIZE * frames_per_block);
+			chunk->size = PAGE_SIZE * frames_per_block;
+			for (size_t n = 1; n < KPM_NORDERS; n++)
+				kpm_update_order(n, chunk->addr, chunk->size);
+			return 0;
+		}
+	}
+	return -1;
+}
+
+void kpm_free(kpm_chunk_t *chunk) {
+	size_t nframes;
+	size_t first_frame_index;
+
+	if (!ISALIGNED(chunk->addr, PAGE_SIZE)) 
+		return;
+
+	first_frame_index = (uintptr_t)chunk->addr / PAGE_SIZE;
+	nframes = ALIGNNEXT(chunk->size, PAGE_SIZE) / PAGE_SIZE;
+	
+	for (size_t i = first_frame_index; i < first_frame_index + nframes; i++) {
+		if (KPM_IS_ENABLED(i))
+			KPM_FREE(0, i);
+	}
+	for (size_t n = 1; n < KPM_NORDERS; n++)
+		kpm_update_order(n, chunk->addr, chunk->size);
 }
