d0a13dd8691d6ac5dbc6b75d1e1650dd3145bf14
lfalkau <lfalkau@student.42.fr>
2022-12-07 10:15:59 +0100

    Adds shell commands to test alloc and free
    The alloc command will choose a pseudo random
    number and allocate a chunk of this size
    
    The free command will free the last allocated
    chunk
    
    Signed-off-by: lfalkau <lfalkau@student.42.fr>

--- kernel/builtins.c
+++ kernel/builtins.c
@@ -6,7 +6,7 @@
  * Handlers for each shell commands.
  *
  * created: 2022/11/17 - mrxx0 <chcoutur@student.42.fr>
- * updated: 2022/11/21 - lfalkau <lfalkau@student.42.fr>
+ * updated: 2022/12/07 - lfalkau <lfalkau@student.42.fr>
  */
 
 #include <kernel/keyboard.h>
@@ -14,6 +14,45 @@
 #include <kernel/print.h>
 #include <kernel/ps2.h>
 #include <kernel/vga.h>
+#include <kernel/kpm.h>
+
+kpm_chunk_t g_chunk;
+
+uint32_t pseudorand(uint32_t min, uint32_t max) {
+	static uint32_t m_seed = 42;
+
+    // Constants for the LCG
+    const uint32_t a = 1103515245;
+    const uint32_t c = 12345;
+    const uint32_t m = (uint32_t)(1 << 16) + 1;
+
+    // Generate the next pseudo-random number
+    m_seed = (a * m_seed + c) % m;
+
+    // Scale the random number to the desired range and return it
+    return (m_seed % (max - min + 1)) + min;
+}
+
+/* Choose a pseudo random size, print it,
+ * allocates a chunk of this size with kpm_alloc
+ * and print the resulting chunk.
+ */
+void alloc() {
+	size_t size;
+	int res;
+
+	size = pseudorand(1, 1 << 16);
+	VGA_setforegroundcolor(VGA_COLOR_GREEN);
+	kprintf("Asked for allocation of %u bytes\n", size);
+	res = kpm_alloc(&g_chunk, size);
+	kprintf("status code: %d\n", res);
+	VGA_setforegroundcolor(VGA_DFL_COLOR);
+	kprintf("chunk: {\n    addr = %p\n    size = %u\n}\n", g_chunk.addr, g_chunk.size);
+}
+
+void free() {
+	kpm_free(&g_chunk);
+}
 
 /* Reboot kernel through the PS/2 controller by sending
  * a reset signal to the CPU as soon as the ps/2 data
