58ecf79a2be198cb293b65346f6b71fe141be8ca
glafond- <glafond-@student.42.fr>
2022-12-07 17:47:09 +0100

    Change the shell to a better one and fix strcmp.
    Create ksh a simple shell with multiple arguments.
    The shell call function found in an array call builtin in ksh.c and look
    for a function with a name corresponmding to the first argument of the
    command.
    To execute a command, the shell translate the string entered by the user
    to a array of string and call the correct function with the array of
    string in argument of the function.
    The shell is not perfectly working, all interaction with the screenbuf
    execpt printing charactere doesn't work.
    
    Fix strcmp: change `return s1 - s2;` to `return *s1 - *s2`.
    
    Signed-off-by: glafond- <glafond-@student.42.fr>

--- include/kernel/builtins.h
+++ include/kernel/builtins.h
@@ -6,23 +6,22 @@
  * Builtins functions for shell.
  *
  * created: 2022/11/17 - mrxx0 <chcoutur@student.42.fr>
- * updated: 2022/11/17 - mrxx0 <chcoutur@student.42.fr>
+ * updated: 2022/12/13 - glafond- <glafond-@student.42.fr>
  */
 
 #ifndef BUILTINS_H
 #define BUILTINS_H
 
-void alloc();
-void free();
-void reboot();
-void poweroff();
-void shell();
-void key_us();
-void key_fr();
-void color_red();
-void color_reset();
-void clear();
-void help();
-void int_divide();
+void alloc(char **argv);
+void free(char **argv);
+void reboot(char **argv);
+void poweroff(char **argv);
+void key_us(char **argv);
+void key_fr(char **argv);
+void color_red(char **argv);
+void color_reset(char **argv);
+void clear(char **argv);
+void help(char **argv);
+void int_divide(char **argv);
 
 #endif
--- kernel/builtins.c
+++ kernel/builtins.c
@@ -6,7 +6,7 @@
  * Handlers for each shell commands.
  *
  * created: 2022/11/17 - mrxx0 <chcoutur@student.42.fr>
- * updated: 2022/12/07 - lfalkau <lfalkau@student.42.fr>
+ * updated: 2022/12/13 - glafond- <glafond-@student.42.fr>
  */
 
 #include <kernel/keyboard.h>
@@ -15,6 +15,11 @@
 #include <kernel/ps2.h>
 #include <kernel/vga.h>
 #include <kernel/kpm.h>
+
+#include <kernel/screenbuf.h>
+
+extern struct screenbuf *sb;
+extern int sb_index;
 
 kpm_chunk_t g_chunk;
 
@@ -42,11 +47,11 @@ void alloc() {
 	int res;
 
 	size = pseudorand(1, 1 << 16);
-	VGA_setforegroundcolor(VGA_COLOR_GREEN);
+	sb_set_fg(sb + sb_index, VGA_COLOR_GREEN);
 	kprintf("Asked for allocation of %u bytes\n", size);
 	res = kpm_alloc(&g_chunk, size);
 	kprintf("status code: %d\n", res);
-	VGA_setforegroundcolor(VGA_DFL_COLOR);
+	sb_set_fg(sb + sb_index, VGA_DFL_COLOR);
 	kprintf("chunk: {\n    addr = %p\n    size = %u\n}\n", g_chunk.addr, g_chunk.size);
 }
 
@@ -58,7 +63,7 @@ void free() {
  * a reset signal to the CPU as soon as the ps/2 data
  * buffer is empty.
  */
-void reboot()
+void reboot(char **argv)
 {
 	while (port_read_u8(PS2_STATUS_REGISTER_PORT) & PS2_INPUT_BUFFER_FULL);
 	port_write_u8(PS2_STATUS_COMMAND_PORT, CPU_RESET);
@@ -67,7 +72,7 @@ void reboot()
 
 /* Poweroff the kernel through the qemu debug port
  */
-void poweroff()
+void poweroff(char **argv)
 {
 	port_write_u16(0x604, 0x2000);
 	asm volatile ("hlt");
@@ -75,49 +80,49 @@ void poweroff()
 
 /* Switch to keyboard to US104 layout.
  */
-void key_us()
+void key_us(char **argv)
 {
 	KBD_setkeymap(US104_getkeymapentry);
 }
 
 /* Switch to keyboard to FR layout.
  */
-void key_fr()
+void key_fr(char **argv)
 {
 	KBD_setkeymap(FR_getkeymapentry);
 }
 
 /* Set foreground color to red.
  */
-void color_red()
+void color_red(char **argv)
 {
-	VGA_setforegroundcolor(VGA_COLOR_RED);
+	sb_set_fg(sb + sb_index, VGA_COLOR_RED);
 }
 
 /* Set foreground color to default one.
  */
-void color_reset()
+void color_reset(char **argv)
 {
-	VGA_setforegroundcolor(VGA_DFL_COLOR);
+	sb_set_color(sb + sb_index, VGA_DFL_COLOR);
 }
 
 /* Generate a divide interrupt.
  */
-void int_divide()
+void int_divide(char **argv)
 {
 	asm volatile ("int $0x0");
 }
 
 /* Clear screen.
  */
-void clear()
+void clear(char **argv)
 {
-	VGA_clear();
+	sb_clear(sb + sb_index);
 }
 
 /* Prints usage and help message.
  */
-void help()
+void help(char **argv)
 {
 	kprintf("KFS - available commands :\n\
     key-us - switch to US104 layout\n\
