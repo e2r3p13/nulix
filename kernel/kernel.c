c2d9cff5e9740fe8cb41821d44112a10b2c91a58
glafond- <glafond-@student.42.fr>
2022-12-12 16:59:55 +0100

    Fix multiboot header
    Try to call a function before paging but can't call kpm_init.
    
    Signed-off-by: glafond- <glafond-@student.42.fr>

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -24,6 +24,8 @@ struct screenbuf sb[NBSCREENBUF];
 struct screenbuf *sb_current;
 int sb_nbscreen = NBSCREENBUF;
 
+extern buddy_t *buddy;
+
 /* Initialize all descriptor tables (gdt, idt, ...)
  *
  */
@@ -33,8 +35,38 @@ static void init_descriptor_tables() {
 	idt_init();
 }
 
-void kernel_main(unsigned long multiboot_info_addr) {
+void kernel_init_kpm(unsigned long multiboot_info_addr) {
+	char cset[] = "0123456789abcdef";
+	uint16_t *vga = (uint16_t*)0xb8000;
+
+	*vga = 0x0f00 | 0x39;
+	vga++;
 	multiboot_info_t *mbi = (multiboot_info_t *)multiboot_info_addr;
+	(&kpm_init - 0xc0000000)((void *)mbi->mmap_addr,
+		mbi->mmap_length / sizeof(struct multiboot_mmap_entry),
+		mbi->mem_upper - mbi->mem_lower);
+
+	uint32_t ptr = (uint32_t)(buddy - 0xc0000000);
+
+	*vga = 0x0f00 | cset[(ptr >> 28) & 0x0f];
+	vga++;
+	*vga = 0x0f00 | cset[(ptr >> 24) & 0x0f];
+	vga++;
+	*vga = 0x0f00 | cset[(ptr >> 20) & 0x0f];
+	vga++;
+	*vga = 0x0f00 | cset[(ptr >> 16) & 0x0f];
+	vga++;
+	*vga = 0x0f00 | cset[(ptr >> 12) & 0x0f];
+	vga++;
+	*vga = 0x0f00 | cset[(ptr >> 8) & 0x0f];
+	vga++;
+	*vga = 0x0f00 | cset[(ptr >> 4) & 0x0f];
+	vga++;
+	*vga = 0x0f00 | cset[(ptr) & 0x0f];
+	vga++;
+}
+
+void kernel_main() {
 
 	init_descriptor_tables();
 	KBD_initialize();
