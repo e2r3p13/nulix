c75ccfca7b021094f252700470d902eb5f3de376
glafond- <glafond-@student.42.fr>
2022-12-15 18:03:01 +0100

    Resolve PR and fix a bug
    Add comments and put some file to the norm.
    
    Use a structure to represent a generic page entry and create two
    specific structure to represent page directory and page table.
    
    Map the page directory to itself instead of using a page table to map all
    other page table.
    
    Remove useless file added by error.
    
    Signed-off-by: glafond- <glafond-@student.42.fr>

--- include/kernel/paging.h
+++ include/kernel/paging.h
@@ -3,22 +3,68 @@
 
 /* include/kernel/paging.h
  *
- *
+ * Header for pagination related things
  *
  * created: 2022/12/12 - xlmod <glafond-@student.42.fr>
- * updated: 2022/12/12 - xlmod <glafond-@student.42.fr>
+ * updated: 2022/12/15 - glafond- <glafond-@student.42.fr>
  */
 
 #ifndef PAGING_H
 #define PAGING_H
 
+#include <stdint.h>
+
 #define PAGE_SIZE 4096
 
-#define PAGE_DIRECTORY_LENGTH	1024
-#define PAGE_TABLE_LENGTH		1024
+#define PAGE_DIRECORY_LENGTH	(PAGE_SIZE / sizeof(uint32_t))
+#define PAGE_TABLE_LENGTH		PAGE_DIRECORY_LENGTH
 
-#define LAST_PAGE_ENTRY	0x3ff
+#define LAST_PAGE_ENTRY			(PAGE_DIRECORY_LENGTH - 1)
 
-void map_page(void *physaddr, void *virtaddr, uint32_t flags);
+struct page_entry {
+	uint32_t present: 1;
+	uint32_t writable: 1;
+	uint32_t user: 1;
+	uint32_t write_through: 1;
+	uint32_t cache_disable: 1;
+	uint32_t accessed: 1;
+	uint32_t specific: 3;
+	uint32_t avl_3: 1;
+	uint32_t avl_4: 1;
+	uint32_t avl_5: 1;
+	uint32_t address: 20;
+};
+
+struct page_directory_entry {
+	uint32_t present: 1;
+	uint32_t writable: 1;
+	uint32_t user: 1;
+	uint32_t write_through: 1;
+	uint32_t cache_disable: 1;
+	uint32_t accessed: 1;
+	uint32_t avl_1: 1;
+	uint32_t page_size: 1;
+	uint32_t avl_2: 1;
+	uint32_t avl_3: 1;
+	uint32_t avl_4: 1;
+	uint32_t avl_5: 1;
+	uint32_t address: 20;
+};
+
+struct page_table_entry {
+	uint32_t present: 1;
+	uint32_t writable: 1;
+	uint32_t user: 1;
+	uint32_t write_through: 1;
+	uint32_t cache_disable: 1;
+	uint32_t accessed: 1;
+	uint32_t dirty: 1;
+	uint32_t page_attribute_table: 1;
+	uint32_t global: 1;
+	uint32_t avl_3: 1;
+	uint32_t avl_4: 1;
+	uint32_t avl_5: 1;
+	uint32_t address: 20;
+};
 
 #endif
--- kernel/boot_init.c
+++ kernel/boot_init.c
@@ -3,10 +3,10 @@
 
 /* kernel/boot_init.c
  *
- *
+ * Initialize temporary boot pages and load them in cr3
  *
  * created: 2022/12/12 - xlmod <glafond-@student.42.fr>
- * updated: 2022/12/12 - xlmod <glafond-@student.42.fr>
+ * updated: 2022/12/15 - glafond- <glafond-@student.42.fr>
  */
 
 #include <stdint.h>
@@ -14,36 +14,44 @@
 #include <kernel/paging.h>
 #include <kernel/kernel.h>
 
-extern uint32_t sk;
-extern uint32_t ek;
-
+/*
+ * Cleat a page entry by setting all bits to 0.
+ */
 #define CLEAR(page, size) \
 for (int i = 0; i < PAGE_SIZE; i++) {\
-	page[i] = 0; \
+	uint32_t *ptr = (uint32_t *)(page + i); \
+	*ptr = 0; \
 }
 
+/*
+ * Initialize the page entry in present and writable and set physical address.
+ */
+#define INIT(page_entry, addr) do {\
+	(page_entry).present = 1; \
+	(page_entry).writable = 1; \
+	(page_entry).address = (addr) >> 12; \
+} while (0)
+
+/*
+ * Create a page directory and a page table and map the first 4M at virtual
+ * address 0x0 and 0xc0000000.
+ */
 void boot_init() {
-	uint32_t *page_directory = (uint32_t *)0x1000;
-	uint32_t *last_page_table = (uint32_t *)0x2000;
-	uint32_t *page_table1 = (uint32_t *)0x3000;
+	struct page_entry *page_directory = (struct page_entry *)0x1000;
+	struct page_entry *page_table1 = (struct page_entry *)0x2000;
 
 	CLEAR(page_directory, PAGE_DIRECTORY_LENGTH);
-	page_directory[PAGE_DIRECTORY_LENGTH - 1] = 0x003 | ((uint32_t)last_page_table & (~0xfff));
-	CLEAR(last_page_table, PAGE_TABLE_LENGTH);
-	last_page_table[PAGE_TABLE_LENGTH - 1] = 0x003 | ((uint32_t)page_directory & (~0xfff));
+	INIT(page_directory[LAST_PAGE_ENTRY], (uint32_t)page_directory);
+
 	CLEAR(page_table1, PAGE_TABLE_LENGTH);
 
-	uint32_t phys_end_kernel = ((uint32_t)&ek - KERNEL_VIRT_OFFSET);
-	for (uint32_t pindex = 0; pindex <= PAGE_SIZE; pindex++)
-		page_table1[pindex] = (pindex * PAGE_SIZE) | 0x003;
+	for (uint32_t pindex = 0; pindex < PAGE_TABLE_LENGTH; pindex++)
+		INIT(page_table1[pindex], pindex * PAGE_SIZE);
 
-	page_directory[0] = (uint32_t)page_table1 | 0x003;
-	last_page_table[0] = (uint32_t)page_table1 | 0x003;
+	INIT(page_directory[0], (uint32_t)page_table1);
+	INIT(page_directory[KERNEL_VIRT_OFFSET >> 22], (uint32_t)page_table1);
 
-	page_directory[KERNEL_VIRT_OFFSET >> 22] = (uint32_t)page_table1 | 0x003;
-	last_page_table[KERNEL_VIRT_OFFSET >> 22] = (uint32_t)page_table1 | 0x003;
-
-
+	// Load the page directory to cr3 to tell the cpu to using this page directory
+	// to resolve virtual address.
 	__asm__ volatile ("movl %0, %%cr3" :: "r" ((uint32_t)page_directory));
-
 }
