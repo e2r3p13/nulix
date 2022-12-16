dd770b58cb75beb57a92c0b0b229c8a7e27f025b
glafond- <glafond-@student.42.fr>
2022-12-16 13:13:21 +0100

    Create page init and clear functions
    Signed-off-by: glafond- <glafond-@student.42.fr>

--- kernel/boot_init.c
+++ kernel/boot_init.c
@@ -6,31 +6,14 @@
  * Initialize temporary boot pages and load them in cr3
  *
  * created: 2022/12/12 - xlmod <glafond-@student.42.fr>
- * updated: 2022/12/15 - glafond- <glafond-@student.42.fr>
+ * updated: 2022/12/16 - glafond- <glafond-@student.42.fr>
  */
 
 #include <stdint.h>
 
 #include <kernel/paging.h>
 #include <kernel/kernel.h>
-
-/*
- * Cleat a page entry by setting all bits to 0.
- */
-#define CLEAR(page, size) \
-for (int i = 0; i < PAGE_SIZE; i++) {\
-	uint32_t *ptr = (uint32_t *)(page + i); \
-	*ptr = 0; \
-}
-
-/*
- * Initialize the page entry in present and writable and set physical address.
- */
-#define INIT(page_entry, addr) do {\
-	(page_entry).present = 1; \
-	(page_entry).writable = 1; \
-	(page_entry).address = (addr) >> 12; \
-} while (0)
+#include <kernel/string.h>
 
 /*
  * Create a page directory and a page table and map the first 4M at virtual
@@ -40,16 +23,16 @@ void boot_init() {
 	struct page_entry *page_directory = (struct page_entry *)0x1000;
 	struct page_entry *page_table1 = (struct page_entry *)0x2000;
 
-	CLEAR(page_directory, PAGE_DIRECTORY_LENGTH);
-	INIT(page_directory[LAST_PAGE_ENTRY], (uint32_t)page_directory);
+	(memset - KERNEL_VIRT_OFFSET)(page_directory, 0, PAGE_DIRECTORY_LENGTH);
+	(page_init - KERNEL_VIRT_OFFSET)(page_directory + LAST_PAGE_ENTRY, page_directory, 1, 0);
 
-	CLEAR(page_table1, PAGE_TABLE_LENGTH);
+	(memset - KERNEL_VIRT_OFFSET)(page_directory, 0, PAGE_DIRECTORY_LENGTH);
 
 	for (uint32_t pindex = 0; pindex < PAGE_TABLE_LENGTH; pindex++)
-		INIT(page_table1[pindex], pindex * PAGE_SIZE);
+		(page_init - KERNEL_VIRT_OFFSET)(page_table1 + pindex, (void *)(pindex * PAGE_SIZE), 1, 0);
 
-	INIT(page_directory[0], (uint32_t)page_table1);
-	INIT(page_directory[KERNEL_VIRT_OFFSET >> 22], (uint32_t)page_table1);
+	(page_init - KERNEL_VIRT_OFFSET)(page_directory, page_table1, 1 ,0);
+	(page_init - KERNEL_VIRT_OFFSET)(page_directory + (KERNEL_VIRT_OFFSET >> 22), page_table1, 1, 0);
 
 	// Load the page directory to cr3 to tell the cpu to using this page directory
 	// to resolve virtual address.
