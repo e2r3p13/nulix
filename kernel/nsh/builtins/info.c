a24acb2ef2db4d5c4c5e34bf2059c851459cc02b
xlmod <glafond-@student.42.fr>
2022-12-17 14:35:33 +0100

    Fix #48 and upgrade info buddy
    In bitmap_ffu, use a bitmap_t(uint8_t) variable to get the inverted byte
    and call __builtin_ffs.
    The old way used a pointer and called `__builtin_ffs(~*cur)` that
    casted the value in 32bit so the __builtin_ffs returned a index bigger
    than the number of blocks.
    
    Add to info buddy a optional arg to print the order.
    
    Reset the color in alloc when allocation failed.
    
    Check on size of alloc and free argument to return an error if negative.
    Also check the address argument of free builtin to return an error if
    not well formatted.
    
    Signed-off-by: xlmod <glafond-@student.42.fr>
    Co-authored-by: mrxx0 <chcoutur@student.42.fr>

--- kernel/nsh/builtins/info.c
+++ kernel/nsh/builtins/info.c
@@ -6,13 +6,15 @@
  * Info builtin file
  *
  * created: 2022/12/09 - mrxx0 <chcoutur@student.42.fr>
- * updated: 2022/12/15 - glafond- <glafond-@student.42.fr>
+ * updated: 2022/12/17 - xlmod <glafond-@student.42.fr>
  */ 
 
 #include <stdint.h>
 #include <kernel/print.h>
 #include <kernel/string.h>
 #include <kernel/kpm.h>
+#include <kernel/screenbuf.h>
+#include <kernel/stdlib.h>
 
 #include "../../gdt_internal.h"
 #include "../../idt_internal.h"
@@ -25,6 +27,8 @@ extern void *stack_top;
 extern buddy_t *buddy;
 extern t_idt_entry idt[256];
 extern t_idt_ptr idtp;
+
+extern struct screenbuf *sb_current;
 
 #define PRINT(reg, var, format) \
 	reg(var); kprintf(format, var)
@@ -62,13 +66,39 @@ static void info_idt() {
 	kprintf("Size = %u bytes\n", sizeof(idt));	
 }
 
-static void info_buddy() {
+static void info_buddy_print_order(int order) {
+	uint8_t oldcolor = sb_get_color(sb_current);
+	kprintf("order %u(size of %u)\n", order, buddy->orders[order].size);
+	for (size_t i = 0; i < buddy->orders[order].size; i++) {
+		if (i % 32 == 0) {
+			kprintf("\n%4x  ", i);
+		}
+		bitmap_t val = buddy->orders[order].bitmap[i];
+		if (val == 0) {
+			sb_set_bg(sb_current, SB_COLOR_WHITE);
+			kprintf("  ");
+		} else if (val == 0xff) {
+			sb_set_bg(sb_current, SB_COLOR_RED);
+			kprintf("  ");
+		} else {
+			sb_set_bg(sb_current, SB_COLOR_BROWN);
+			kprintf("%2x", val);
+		}
+		sb_set_color(sb_current, oldcolor);
+	}
+	sb_set_color(sb_current, oldcolor);
+	kprintf("\n");
+}
+
+static void info_buddy(int order) {
 	kprintf("INFO BUDDY\n");
 	kprintf("buddy address:        %p\n", buddy);
 	kprintf("buddy size:           %u KB\n", buddy->size / 1024);
 	kprintf("orders address:       %p\n", buddy->orders[0].bitmap);
 	kprintf("frame number:         %x\n", buddy->nframes);
-	kprintf("memory size:          %u KB\n\n", buddy->nframes << 2);
+	kprintf("memory size:          %u KB\n", buddy->nframes << 2);
+	if (order >= 0)
+		info_buddy_print_order(order);
 }
 
 static void info_stack() {
@@ -103,7 +133,17 @@ int info(int argc, char **argv) {
 	} else if (!strcmp(argv[1], "stack")) {
 		info_stack();
 	} else if (!strcmp(argv[1], "buddy")) {
-		info_buddy();
+		if (argc > 2) {
+			char *ptr;
+			int order = strtol(argv[2], &ptr, 0);
+			if (*ptr || order < 0 || order >= KPM_NORDERS) {
+				kprintf(BLTNAME ": order number wrong.\n");
+				return -1;
+			}
+			info_buddy(order);
+		} else {
+			info_buddy(-1);
+		}
 	} else if (!strcmp(argv[1], "idt")) {
 		info_idt();
 	} else if (!strcmp(argv[1], "registers")) {
