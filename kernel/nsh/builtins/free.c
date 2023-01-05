aa98053637f29b6f497347f0ed02ce138c540b2c
mrxx0 <chcoutur@student.42.fr>
2023-01-05 10:50:25 +0100

    Fix compilation error
    Signed-off-by: mrxx0 <chcoutur@student.42.fr>

--- kernel/nsh/builtins/free.c
+++ kernel/nsh/builtins/free.c
@@ -6,7 +6,7 @@
  * Free builtin file
  *
  * created: 2022/12/15 - mrxx0 <chcoutur@student.42.fr>
- * updated: 2023/01/04 - mrxx0 <chcoutur@student.42.fr>
+ * updated: 2023/01/05 - mrxx0 <chcoutur@student.42.fr>
  */
 
 #include <kernel/kpm.h>
@@ -19,7 +19,7 @@
 extern struct screenbuf sb[];
 extern struct screenbuf *sb_current;
 
-extern const int MAXALLOC;
+#define MAXALLOC 10
 extern struct kpm_chunk_head chunk_heads[];
 extern int nalloc;
 
@@ -36,15 +36,15 @@ int free(int argc, char **argv) {
 		return -1;
 	}
 	char *ptr;
-	uint32_t addr = strtol(argv[1], &ptr, 0);
+	uint32_t to_free = strtol(argv[1], &ptr, 0);
 	if (*ptr != 0 || argv[1][0] == '-') {
 		kprintf(BLTNAME ": Index not well formatted.\n");
 		return -1;
 	}
-	if (addr >= MAXALLOC) {
+	if (to_free >= MAXALLOC) {
 		kprintf(BLTNAME ": Index too big.\n");
 		return -1;
 	}
-	kpm_free(&chunk_heads[nalloc]);
+	kpm_free(&chunk_heads[to_free]);
 	return 0;
 }
