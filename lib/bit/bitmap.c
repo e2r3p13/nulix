d86dd64fa8e8ebe1f27bb0a2d3d71c14272b273f
xlmod <glafond-@student.42.fr>
2023-01-05 23:23:14 +0100

    TMP
    Fix bug in kpm in bitmap initialization.
    Add functions bitmap_get_{next|prev}_{one|zero} and change function
    bitmap_get_{last|first}_{one|zero} to wrapper of the next/prev one.
    
    Signed-off-by: xlmod <glafond-@student.42.fr>

--- lib/bit/bitmap.c
+++ lib/bit/bitmap.c
@@ -6,7 +6,7 @@
  * Bitmap struct functions
  *
  * created: 2023/01/05 - glafond- <glafond-@student.42.fr>
- * updated: 2023/01/05 - glafond- <glafond-@student.42.fr>
+ * updated: 2023/01/05 - xlmod <glafond-@student.42.fr>
  */
 
 #include <kernel/bitmap.h>
@@ -69,17 +69,33 @@ int bitmap_get_at(struct bitmap *bitmap, size_t index) {
 	return (bitmap->array[array_index] & (1 << bit_index)) != 0;
 }
 
-int bitmap_get_first_one(struct bitmap *bitmap) {
-	for (int i = 0; (size_t)i < ((bitmap->len - 1) / 8); i++) {
-		uint8_t byte = bitmap->array[i];
+int bitmap_get_next_one(struct bitmap *bitmap, size_t index) {
+	if (index >= bitmap->len)
+		return -1;
+	int i = (int)(index / 8);
+	uint8_t byte = bitmap->array[i];
+	byte &= ((size_t)(~0) << (index % 8));
+	if (byte)
+		return __builtin_ctz(byte) + (i * 8);
+	i++;
+	for (; (size_t)i < ((bitmap->len - 1) / 8); i++) {
+		byte = bitmap->array[i];
 		if (byte)
 			return __builtin_ctz(byte) + (i * 8);
 	}
 	return -1;
 }
 
-int bitmap_get_last_one(struct bitmap *bitmap) {
-	for (int i = (int)((bitmap->len - 1) / 8); i >= 0; i--) {
+int bitmap_get_prev_one(struct bitmap *bitmap, size_t index) {
+	if (index >= bitmap->len)
+		return -1;
+	int i = (int)(index / 8);
+	uint8_t byte = bitmap->array[i];
+	byte &= (~((size_t)(~0) << (index % 8)));
+	if (byte)
+		return ((__builtin_clz(byte) + 1) - 8) + (i * 8);
+	i--;
+	for (; i >= 0; i--) {
 		uint8_t byte = bitmap->array[i];
 		if (byte)
 			return ((__builtin_clz(byte) + 1) - 8) + (i * 8);
@@ -87,10 +103,24 @@ int bitmap_get_last_one(struct bitmap *bitmap) {
 	return -1;
 }
 
-int bitmap_get_first_zero(struct bitmap *bitmap) {
-	uint8_t byte;
-	int i;
-	for (i = 0; (size_t)i < ((bitmap->len - 1) / 8) - 1; i++) {
+int bitmap_get_first_one(struct bitmap *bitmap) {
+	return bitmap_get_next_one(bitmap, 0);
+}
+
+int bitmap_get_last_one(struct bitmap *bitmap) {
+	return bitmap_get_prev_one(bitmap, bitmap->len - 1);
+}
+
+int bitmap_get_next_zero(struct bitmap *bitmap, size_t index) {
+	if (index >= bitmap->len)
+		return -1;
+	int i = (int)(index / 8);
+	uint8_t byte = bitmap->array[i];
+	byte = (~byte) & ((size_t)(~0) << (index % 8));
+	if (byte)
+		return __builtin_ctz(byte) + (i * 8);
+	i++;
+	for (; (size_t)i < ((bitmap->len - 1) / 8) - 1; i++) {
 		byte = bitmap->array[i];
 		byte = (~byte);
 		if (byte)
@@ -103,11 +133,13 @@ int bitmap_get_first_zero(struct bitmap *bitmap) {
 	return -1;
 }
 
-int bitmap_get_last_zero(struct bitmap *bitmap) {
+int bitmap_get_prev_zero(struct bitmap *bitmap, size_t index) {
 	uint8_t byte;
-	int i = (int)(bitmap->len - 1) / 8;
+	if (index >= bitmap->len)
+		return -1;
+	int i = (int)(index / 8);
 	byte = bitmap->array[i];
-	byte = (~byte) & (~((size_t)(~0) << bitmap->len % 8));
+	byte = (~byte) & (~((size_t)(~0) << (index % 8)));
 	if (byte)
 		return ((__builtin_clz(byte) + 1) - 8) + (i * 8);
 	i--;
@@ -119,3 +151,12 @@ int bitmap_get_last_zero(struct bitmap *bitmap) {
 	}
 	return -1;
 }
+
+int bitmap_get_first_zero(struct bitmap *bitmap) {
+	return bitmap_get_next_zero(bitmap, 0);
+}
+
+int bitmap_get_last_zero(struct bitmap *bitmap) {
+	return bitmap_get_prev_zero(bitmap, bitmap->len - 1);
+}
+
